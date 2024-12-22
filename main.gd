5abd96053765e717e2774d3c661325a7fc4c29c5
mortezamohiuodin <mortezamohiuodin@gmail.com>
2024-12-23 00:09:10 +0330

    complete HUD

--- main.gd
+++ main.gd
@@ -2,6 +2,9 @@ extends Node
 @export var rock_scene : PackedScene
 
 var screensize = Vector2.ZERO
+var level = 0
+var score = 0
+var playing = false
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
@@ -21,7 +24,6 @@ func spawn_rock(size, pos=null, vel=null):
 	r.exploded.connect(self._on_rock_exploded)
 
 func _on_rock_exploded(size, radius, pos, vel):
-	print("on_exploded")
 	if size <= 1:
 		return
 	for offset in [-1, 1]:
@@ -29,3 +31,34 @@ func _on_rock_exploded(size, radius, pos, vel):
 		var newpos = pos + dir * radius
 		var newvel = dir * vel.length() * 1.1
 		spawn_rock(size - 1, newpos, newvel)
+		
+func new_game():
+	# remove any old rocks from previous game
+	get_tree().call_group("rocks", "queue_free")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
+
+func _process(delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
+		
+func game_over():
+	playing = false
+	$HUD.game_over()
+
+
+func update_lives() -> void:
+	pass # Replace with function body.
--- player.gd
+++ player.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+signal lives_changed
+signal dead
+
 enum { INIT , ALIVE , INVULNERABLE, DEAD }	
 
 @export var engine_power = 500
@@ -8,6 +11,9 @@ enum { INIT , ALIVE , INVULNERABLE, DEAD }
 @export var bullet_scene : PackedScene
 @export var fire_rate = 0.25
 
+
+var reset_pos = false
+var lives = 0: set = set_lives
 var state = INIT 
 var thrust = Vector2.ZERO
 var rotation_dir = 0
@@ -33,22 +39,11 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
-
-func change_state(new_state):
-	match new_state:
-		INIT:
-			$CollisionShape2D.set_deferred("disabled",true)
-		ALIVE:
-			$CollisionShape2D.set_deferred("disabled",false)
-		INVULNERABLE: 
-			$CollisionShape2D.set_deferred("disabled",true)
-		DEAD:
-			$CollisionShape2D.set_deferred("disabled", true)
 	
-	state = new_state 
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
 
-
-	
 func get_input():
 	thrust = Vector2.ZERO
 	if state in [DEAD,INIT]:
@@ -70,3 +65,40 @@ func shoot():
 	
 func _on_gun_cooldown_timeout():
 	can_shoot = true
+
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+func change_state(new_state):
+	match new_state:
+		INIT:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.modulate.a = 0.5
+		ALIVE:
+			$CollisionShape2D.set_deferred("disabled",false)
+			$Sprite2D.modulate.a = 1.0
+		INVULNERABLE:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
+		DEAD:
+			$CollisionShape2D.set_deferred("disabled",true)
+			$Sprite2D.hide()
+	linear_velocity = Vector2.ZERO
+	dead.emit()
+	state = new_state
+
+
+func _on_invulnerability_timer_timeout() -> void:
+	change_state(ALIVE)
